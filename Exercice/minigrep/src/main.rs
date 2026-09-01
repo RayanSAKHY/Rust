f713bc610305719e7bf5760b3aee7f8caa4be9d9
rasakhy <rayan.sakhy@etu.isima.fr>
2026-09-01 18:26:55 +0200

    Ameilloration du projet minigrep

--- Exercice/minigrep/src/main.rs
+++ Exercice/minigrep/src/main.rs
@@ -11,9 +11,7 @@ pub struct Config {
 }
 
 fn main() {
-    let args: Vec<String> = env::args().collect();
-
-    let config = Config::build(&args).unwrap_or_else(|err| {
+    let config = Config::build(env::args()).unwrap_or_else(|err| {
         eprintln!("Problem parsing arguments: {err}");
         process::exit(1);
     });
@@ -41,22 +39,26 @@ fn run(config: Config) -> Result<(), Box<dyn Error>> {
 }
 
 impl Config {
-    fn build(args: &[String]) -> Result<Config, &'static str> {
+    fn build(mut args: impl Iterator<Item = String>) -> Result<Config, &'static str> {
         let ignore_case: bool;
-        if args.len() < 3 {
-            return Err("not enough arguments");
-        } else if args.len() == 3 {
-            ignore_case = env::var("IGNORE_CASE").is_ok();
-        } else {
-            if args[3].eq("1") {
-                ignore_case = true;
-            } else {
-                ignore_case = false;
-            }
-        }
 
-        let query = args[1].clone();
-        let file_path = args[2].clone();
+        args.next();
+
+        let query = match args.next() {
+            Some(arg) => arg,
+            None => return Err("Didn't get a query string"),
+        };
+
+        let file_path = match args.next() {
+            Some(arg) => arg,
+            None => return Err("Didn't get a file path"),
+        };
+
+        if args.next().is_some_and(|x| x == "1") {
+            ignore_case = true;
+        } else {
+            ignore_case = env::var("IGNORE_CASE").is_ok();
+        }
 
         Ok(Config {
             query,
